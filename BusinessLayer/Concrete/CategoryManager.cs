36bab56795a2131ba9a1b96d93d705aedbc7ff90
emirgzc <83915547+emirgzc@users.noreply.github.com>
2021-06-30 11:35:51 +0300

    MvcKamp

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repocat = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repocat.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName=="" || p.CategoryName.Length <=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repocat.Insert(p);
+            }
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        [Key]
+        public int HeadingID { get; set; }
+
+        [StringLength(80)]
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public ICollection<Content> Contents { get; set; } /* bir başlık birden fazla yazıda olabilir*/
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
